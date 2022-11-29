9736b438fa72f5a8dc882ed65339818ef4f66979
Yamazaki mao <m.s0617r.e@gmail.com>
2022-11-29 18:05:15 +0900

    動きを実装

--- index.js
+++ index.js
@@ -47,3 +47,32 @@ var pos = $(elmHash).offset().top-headerH;//header分の高さを引いた高さ
 $('body,html').animate({scrollTop: pos}, 1000);
 return false;
 });
+
+//ふわっと表示
+function load_effect() {
+    var element = document.getElementsByClassName('load-fade');
+    if(!element) return; // 要素がない場合は終了
+    
+    for(var i = 0; i < element.length; i++) { 
+      element[i].classList.add('is-show');
+    }
+  }
+  setTimeout(load_effect, 600); // 600ミリ秒経過後に実行
+
+
+  function scroll_effect() {
+    var element = document.getElementsByClassName('scroll-up');
+    if(!element) return;
+                        
+    var scrollY = window.pageYOffset;
+    var windowH = window.innerHeight;
+    var showTiming = 200; // 要素を表示するタイミング
+    for(var i = 0; i < element.length; i++) { 
+      var elemClientRect = element[i].getBoundingClientRect(); 
+      var elemY = scrollY + elemClientRect.top; 
+      if(scrollY > elemY - windowH + showTiming) {
+        element[i].classList.add('is-show');
+      }
+    }
+  }
+  window.addEventListener('scroll', scroll_effect); // スクロール時に実行
